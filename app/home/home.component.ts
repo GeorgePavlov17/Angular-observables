85cf6d9bdbc6510f0fae606d5314e6d96e5603bc
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-15 13:01:20 +0300

    building a custom Observable

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +13,19 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.countSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.countSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.countSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
     });
   }
 
