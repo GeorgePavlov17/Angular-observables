9d792ce88666b0f0ed6e63953fbefd0287a27c29
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-16 21:10:57 +0300

    creating a custom reactive validators

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -28,13 +28,14 @@ export class HomeComponent implements OnInit, OnDestroy {
   // Reactive Forms
   gendersReactive = ['male', 'female'];
   signupFormReactive: FormGroup;
+  forbiddenUsernames = ['Chriss', 'Anna'];
 
   constructor() { }
 
   ngOnInit() {
     this.signupFormReactive = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -121,6 +122,13 @@ export class HomeComponent implements OnInit, OnDestroy {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupFormReactive.get('hobbies')).push(control)
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 
   ngOnDestroy() {
