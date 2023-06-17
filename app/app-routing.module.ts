286d5f6b72146c8fb63c53c0dfaaceb213438242
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-17 17:15:56 +0300

    redirecting and wild card route

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -17,7 +18,8 @@ const routes: Routes = [
     {path: ':id', component: ServerComponent},
     {path: ':id/edit', component: EditServerComponent}
   ]},
- 
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' },
 ];
 
 @NgModule({
