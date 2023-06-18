4d901901e81aae0db264d3e6a6b67425fa0a84e4
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-18 22:40:46 +0300

    sending POST and GET request

---
+++ app/http/http.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-http',
+  templateUrl: './http.component.html',
+  styleUrls: ['./http.component.css']
+})
+export class HttpComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http.post('https://angular-http-a91bb-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://angular-http-a91bb-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+      console.log(posts);
+    });
+  }
+
+}
