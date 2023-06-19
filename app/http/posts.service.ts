7ddb274beba15f5da3444cf4e012bdbf97cbc121
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-19 12:51:03 +0300

    Different Types of Responses

--- app/http/posts.service.ts
+++ app/http/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from "rxjs";
 
 @Injectable({providedIn: 'root'})
@@ -16,7 +16,11 @@ export class PostsService {
     createAndStorePost(title: string, content: string) {
         const postData: Post = { title: title, content: content };
 
-        this.http.post<{name: string}>('https://angular-http-a91bb-default-rtdb.firebaseio.com/posts.json', postData)
+        this.http.post<{name: string}>('https://angular-http-a91bb-default-rtdb.firebaseio.com/posts.json', postData,
+            {
+                observe: 'response'
+            }
+        )
         .subscribe(responseData => {
             console.log(responseData);
         }, error => {
@@ -51,7 +55,21 @@ export class PostsService {
     }
 
     deletePosts() {
-        return this.http.delete('https://angular-http-a91bb-default-rtdb.firebaseio.com/posts.json');
+        return this.http.delete('https://angular-http-a91bb-default-rtdb.firebaseio.com/posts.json', 
+            {
+                observe: 'events'
+            }
+        ).pipe(
+            tap(event => {
+                console.log(event);
+                if(event.type === HttpEventType.Sent) {
+                    //...
+                }
+                if(event.type === HttpEventType.Response) {
+                    console.log(event.body);
+                }
+            })
+        );
     }
 
 }
